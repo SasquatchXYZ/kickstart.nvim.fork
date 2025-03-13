5708bbd94ef2eb66731b41bb237f99c54043096f
Dalton Ricker <40616938+SasquatchXYZ@users.noreply.github.com>
2025-03-13 18:00:48 -0400

    Playing around with different themes

--- lua/kickstart/plugins/colorscheme.lua
+++ lua/kickstart/plugins/colorscheme.lua
@@ -1,120 +1,56 @@
---[[
 return {
-  -- You can easily change to a different colorscheme.
-  -- Change the name of the colorscheme plugin below, and then
-  -- change the command in the config to whatever the name of that colorscheme is.
-  --
-  -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-  'folke/tokyonight.nvim',
-  priority = 1000, -- Make sure to load this before all the other start plugins.
-  config = function()
-    ---@diagnostic disable-next-line: missing-fields
-    require('tokyonight').setup {
-      styles = {
-        comments = { italic = false }, -- Disable italics in comments
-      },
-    }
-
-    -- Load the colorscheme here.
-    -- Like many other themes, this one has different styles, and you could load
-    -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-    vim.cmd.colorscheme 'tokyonight-night'
-  end,
-}
-]]
---
-
-return {
-  'marko-cerovac/material.nvim',
+  'catppuccin/nvim',
+  name = 'catppuccin',
   priority = 1000,
   config = function()
-    require('material').setup {
-
-      contrast = {
-        terminal = false, -- Enable contrast for the built-in terminal
-        sidebars = false, -- Enable contrast for sidebar-like windows ( for example Nvim-Tree )
-        floating_windows = false, -- Enable contrast for floating windows
-        cursor_line = false, -- Enable darker background for the cursor line
-        lsp_virtual_text = false, -- Enable contrasted background for lsp virtual text
-        non_current_windows = false, -- Enable contrasted background for non-current windows
-        filetypes = {}, -- Specify which filetypes get the contrasted (darker) background
+    require('catppuccin').setup {
+      flavour = 'auto', -- latte, frappe, macchiato, mocha
+      background = { -- :h background
+        light = 'latte',
+        dark = 'mocha',
       },
-
-      styles = { -- Give comments style such as bold, italic, underline etc.
-        comments = { --[[ italic = true ]]
-        },
-        strings = { --[[ bold = true ]]
-        },
-        keywords = {
-          italic = true,
-        },
-        functions = { --[[ bold = true, undercurl = true ]]
-        },
+      transparent_background = false, -- disables setting the background color.
+      show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+      term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+      dim_inactive = {
+        enabled = false, -- dims the background color of inactive window
+        shade = 'dark',
+        percentage = 0.15, -- percentage of the shade to apply to the inactive window
+      },
+      no_italic = false, -- Force no italic
+      no_bold = false, -- Force no bold
+      no_underline = false, -- Force no underline
+      styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+        comments = { 'italic' }, -- Change the style of comments
+        conditionals = { 'italic' },
+        loops = {},
+        functions = {},
+        keywords = { 'italic' },
+        strings = {},
         variables = {},
-        operators = {
-          italic = true,
+        numbers = {},
+        booleans = {},
+        properties = { 'italic' },
+        types = {},
+        operators = {},
+        -- miscs = {}, -- Uncomment to turn off hard-coded styles
+      },
+      color_overrides = {},
+      custom_highlights = {},
+      default_integrations = true,
+      integrations = {
+        cmp = true,
+        gitsigns = true,
+        nvimtree = true,
+        treesitter = true,
+        notify = false,
+        mini = {
+          enabled = true,
+          indentscope_color = '',
         },
-        types = {
-          italic = true,
-        },
+        -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
       },
-
-      plugins = { -- Uncomment the plugins that you use to highlight them
-        -- Available plugins:
-        -- "coc",
-        -- "colorful-winsep",
-        -- "dap",
-        -- "dashboard",
-        -- "eyeliner",
-        -- "fidget",
-        -- "flash",
-        'gitsigns',
-        'harpoon',
-        -- "hop",
-        -- "illuminate",
-        -- "indent-blankline",
-        -- "lspsaga",
-        'mini',
-        -- "neogit",
-        -- "neotest",
-        -- "neo-tree",
-        -- "neorg",
-        -- "noice",
-        -- "nvim-cmp",
-        -- "nvim-navic",
-        'nvim-tree',
-        'nvim-web-devicons',
-        -- "rainbow-delimiters",
-        -- "sneak",
-        'telescope',
-        -- "trouble",
-        'which-key',
-        -- "nvim-notify",
-      },
-
-      disable = {
-        colored_cursor = false, -- Disable the colored cursor
-        borders = false, -- Disable borders between vertically split windows
-        background = false, -- Prevent the theme from setting the background (NeoVim then uses your terminal background)
-        term_colors = false, -- Prevent the theme from setting terminal colors
-        eob_lines = false, -- Hide the end-of-buffer lines
-      },
-
-      high_visibility = {
-        lighter = false, -- Enable higher contrast text for lighter style
-        darker = true, -- Enable higher contrast text for darker style
-      },
-
-      lualine_style = 'default', -- Lualine style ( can be 'stealth' or 'default' )
-
-      async_loading = true, -- Load parts of the theme asynchronously for faster startup (turned on by default)
-
-      custom_colors = nil, -- If you want to override the default colors, set this to a function
-
-      custom_highlights = {}, -- Overwrite highlights with your own
     }
-
-    vim.g.material_style = 'deep ocean'
-    vim.cmd.colorscheme 'material'
+    vim.cmd.colorscheme 'catppuccin-mocha'
   end,
 }
