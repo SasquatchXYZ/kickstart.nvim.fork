2693a16655e331c4e5e3b76969d16e2366d9f202
Dalton Ricker <40616938+SasquatchXYZ@users.noreply.github.com>
2025-03-14 13:57:38 -0400

    Flow.nvim

--- lua/kickstart/plugins/colorscheme.lua
+++ lua/kickstart/plugins/colorscheme.lua
@@ -1,14 +1,28 @@
 return {
-  'folke/tokyonight.nvim',
+  '0xstepit/flow.nvim',
+  lazy = false,
   priority = 1000,
+  tag = 'v2.0.2',
   config = function()
-    require('tokyonight').setup {
-      styles = {
-        comments = { italic = false }, -- Disable italics in comments
+    require('flow').setup {
+      theme = {
+        style = 'dark', --  "dark" | "light"
+        contrast = 'high', -- "default" | "high"
+        transparent = false, -- true | false
+      },
+      colors = {
+        mode = 'default', -- "default" | "dark" | "light"
+        fluo = 'pink', -- "pink" | "cyan" | "yellow" | "orange" | "green"
+        custom = {
+          saturation = '', -- "" | string representing an integer between 0 and 100
+          light = '', -- "" | string representing an integer between 0 and 100
+        },
+      },
+      ui = {
+        borders = 'inverse', -- "theme" | "inverse" | "fluo" | "none"
+        aggressive_spell = false, -- true | false
       },
     }
-
-    -- Load the colorscheme here.
-    vim.cmd.colorscheme 'tokyonight-night'
+    vim.cmd 'colorscheme flow'
   end,
 }
