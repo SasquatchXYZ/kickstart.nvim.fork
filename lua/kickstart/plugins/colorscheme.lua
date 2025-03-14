639ff1e1700763c148a0a68a5587043a6a6fbd60
Dalton Ricker <40616938+SasquatchXYZ@users.noreply.github.com>
2025-03-13 22:10:00 -0400

    TokyoDark

--- lua/kickstart/plugins/colorscheme.lua
+++ lua/kickstart/plugins/colorscheme.lua
@@ -1,56 +1,26 @@
 return {
-  'catppuccin/nvim',
-  name = 'catppuccin',
+  'tiagovla/tokyodark.nvim',
   priority = 1000,
-  config = function()
-    require('catppuccin').setup {
-      flavour = 'auto', -- latte, frappe, macchiato, mocha
-      background = { -- :h background
-        light = 'latte',
-        dark = 'mocha',
-      },
-      transparent_background = false, -- disables setting the background color.
-      show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
-      term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
-      dim_inactive = {
-        enabled = false, -- dims the background color of inactive window
-        shade = 'dark',
-        percentage = 0.15, -- percentage of the shade to apply to the inactive window
-      },
-      no_italic = false, -- Force no italic
-      no_bold = false, -- Force no bold
-      no_underline = false, -- Force no underline
-      styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
-        comments = { 'italic' }, -- Change the style of comments
-        conditionals = { 'italic' },
-        loops = {},
-        functions = {},
-        keywords = { 'italic' },
-        strings = {},
-        variables = {},
-        numbers = {},
-        booleans = {},
-        properties = { 'italic' },
-        types = {},
-        operators = {},
-        -- miscs = {}, -- Uncomment to turn off hard-coded styles
-      },
-      color_overrides = {},
-      custom_highlights = {},
-      default_integrations = true,
-      integrations = {
-        cmp = true,
-        gitsigns = true,
-        nvimtree = true,
-        treesitter = true,
-        notify = false,
-        mini = {
-          enabled = true,
-          indentscope_color = '',
-        },
-        -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
-      },
-    }
-    vim.cmd.colorscheme 'catppuccin-mocha'
+  opts = {
+    transparent_background = false, -- set background to transparent
+    gamma = 1.00, -- adjust the brightness of the theme
+    styles = {
+      comments = { italic = true }, -- style for comments
+      keywords = { italic = true }, -- style for keywords, and Types in C#
+      identifiers = { italic = true }, -- style for identifiers
+      functions = {}, -- style for functions
+      variables = {}, -- style for variables
+    },
+    custom_highlights = {} or function(highlights, palette)
+      return {}
+    end, -- extend highlights
+    custom_palette = {} or function(palette)
+      return {}
+    end, -- extend palette
+    terminal_colors = true, -- enable terminal colors
+  },
+  config = function(_, opts)
+    require('tokyodark').setup(opts) -- calling setup is optional
+    vim.cmd [[colorscheme tokyodark]]
   end,
 }
