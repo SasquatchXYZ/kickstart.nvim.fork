44845b5812dc5f1ad72650a6ca70bf66132d1fb7
Dalton Ricker <40616938+SasquatchXYZ@users.noreply.github.com>
2025-03-15 23:22:13 -0400

    Update indent-line colors

--- lua/kickstart/plugins/indent_line.lua
+++ lua/kickstart/plugins/indent_line.lua
@@ -6,7 +6,14 @@ return {
     main = 'ibl',
     opts = function(_, opts)
       -- other blankline configuration here
-      return require('indent-rainbowline').make_opts(opts)
+      return require('indent-rainbowline').make_opts(opts, {
+        -- How transparent should the rainbow colors be.  1 is completely opaque, 0 is invisible. 0.07 by default
+        color_transparency = 0.07,
+        -- The 24-bit colors to mix with the background.  Specified in hex.
+        -- { 0xffff40, 0x79ff79, 0xff79ff, 0x4fecec, } by default
+        --   Yellow  ,  Green  ,  Purple ,   Blue  ,
+        colors = { 0xFFCB6B, 0xC3E88D, 0xC792EA, 0x82AAFF },
+      })
     end,
     dependencies = {
       'TheGLander/indent-rainbowline.nvim',
